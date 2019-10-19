c0ace25f11ad6d356f58e98c0e256f9c2581e28d
Joseph <Joseph.siyi@gmail.com>
2019-10-19 16:53:33 +0800

    拜大神

--- Lab1/ws3/lib/ws3-stack.ts
+++ Lab1/ws3/lib/ws3-stack.ts
@@ -1,30 +1,38 @@
 import cdk = require('@aws-cdk/core');
 import ec2 = require('@aws-cdk/aws-ec2');
+import iam = require('@aws-cdk/aws-iam');
 
-import ecs = require('@aws-cdk/aws-ecs');
+import eks = require('@aws-cdk/aws-eks');
 import ecsPatterns = require('@aws-cdk/aws-ecs-patterns')
+import { ImagePullPrincipalType } from '@aws-cdk/aws-codebuild';
+import { EksOptimizedImage } from '@aws-cdk/aws-eks';
+import { InstanceType } from '@aws-cdk/aws-ec2';
 
 export class Ws3Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const app = new cdk.App();
+    const env = {
+      region: 'us-east-1',
+      account: '374801192098'
+    };
 
-    // The code that defines your stack goes here
-    const vpc = new ec2.Vpc(this, 'NewVpc', {
-      cidr: '192.168.0.0/16',
-      natGateways: 1
+    const stack = new cdk.Stack(app, 'EksStack', { env });
+    const vpc = new ec2.Vpc(stack, 'NewVpc', {
+      cidr: '192.168.0.0/16'
+    })   
+
+    // create admin
+    const clusterAdmin = new iam.Role(stack, 'AdminRole', {
+      assumedBy: new iam.AccountRootPrincipal()
     })
-    const cluster = new ecs.Cluster(this, 'Cluser', {vpc})
 
-    const taskDefinition = new ecs.Ec2TaskDefinition(this, 'Task', {
+    const cluster = new eks.Cluster(stack, 'EksCluster', {
+      vpc,
+      defaultCapacity: 1,
+      mastersRole: clusterAdmin,
+      defaultCapacityInstance: new InstanceType('t3.large'),
+      outputClusterName: true,
     })
-
-    const web = taskDefinition.addContainer('web', {
-      image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
-    }) 
-
-    new ecsPatterns.ApplicationLoadBalancedEc2Service(this, 'Service4', {
-      cluster,
-      taskDefinition
-    });
   }
 }
--- Lab1/ws4/lib/ws4-stack.ts
+++ Lab1/ws4/lib/ws4-stack.ts
@@ -1,9 +1,48 @@
 import cdk = require('@aws-cdk/core');
+import { Vpc } from '@aws-cdk/aws-ec2';
+import { Cluster, TaskDefinition, Compatibility, ContainerImage } from '@aws-cdk/aws-ecs'
+import { ApplicationLoadBalancedFargateService } from '@aws-cdk/aws-ecs-patterns';
 
 export class Ws4Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    const app = new cdk.App();
+    const env = {
+      region: 'us-east-1',
+      account: '374801192098'
+    };
+
+    const stack = new cdk.Stack(app, 'FargateAlbSvc', { env });
+    const vpc = new Vpc(stack, 'NewVpc', {
+      cidr: '192.168.0.0/16'
+    })    
+    const cluster = new Cluster(stack, 'Cluster', { vpc });
+
+    const taskDefinition = new TaskDefinition(stack, 'Task', {
+      compatibility: Compatibility.FARGATE,
+      memoryMiB: '512',
+      cpu: '256'
+    });
+
+
+    taskDefinition
+      .addContainer('flask', {
+        image: ContainerImage.fromRegistry('pahud/amazon-ecs-flask-sample'),
+        // image: ContainerImage.fromAsset(path.join(__dirname, '../../python/flask-docker-app/')),
+        environment: {
+          'PLATFORM': `AWS Fargate(${stack.region})`
+        }
+      })
+      .addPortMappings({
+        containerPort: 5000
+      });
+
+    const svc = new ApplicationLoadBalancedFargateService(stack, 'FargateService', {
+      cluster,
+      taskDefinition
+    });
   }
 }
